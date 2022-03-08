c3574cb5a19b1f91377a8eb3c2c5a50f1e974628
W1nter-isHere <W1nter.IsH3re@gmail.com>
2022-03-08 18:29:50 -0500

    Given initial character values

--- dndman/database/users.py
+++ dndman/database/users.py
@@ -1,33 +1,8 @@
 from __future__ import annotations
+
 from enum import Enum
 from typing import Dict
 
-class Character:
-    portrait: str
-    
-    character_name: str
-    player_name: str
-
-    armor_class: int
-    intiative: int
-    speed: int
-    hit_points: int
-
-    strength_value: CharacterValue
-    dexerity_value: CharacterValue
-    contitution_value: CharacterValue
-    intelligence_value: CharacterValue
-    wisdom_value: CharacterValue
-    charisma_value: CharacterValue
-
-    strength_saving_throw: int
-    dexterity_saving_throw: int
-    constitution_saving_throw: int
-    intelligence_saving_throw: int
-    wisdom_saving_throw: int
-    charisma_saving_throw: int
-
-    
 class CharacterValue:
     value: int
     modifier: int
@@ -59,3 +34,28 @@ class EStrengthTypes(Enum):
     INTELLIGENCE = 3,
     WISDOM = 4,
     CHARISMA = 5;
+
+class Character:
+    portrait: str
+    
+    character_name: str
+    player_name: str
+
+    armor_class: int
+    intiative: int
+    speed: int
+    hit_points: int
+
+    strength_value = CharacterValue(EStrengthTypes.STRENGTH)
+    dexerity_value = CharacterValue(EStrengthTypes.DEXTERITY)
+    contitution_value = CharacterValue(EStrengthTypes.CONSTITUTION)
+    intelligence_value = CharacterValue(EStrengthTypes.INTELLIGENCE)
+    wisdom_value = CharacterValue(EStrengthTypes.WISDOM)
+    charisma_value = CharacterValue(EStrengthTypes.CHARISMA)
+
+    strength_saving_throw: int
+    dexterity_saving_throw: int
+    constitution_saving_throw: int
+    intelligence_saving_throw: int
+    wisdom_saving_throw: int
+    charisma_saving_throw: int
